d6117455234c266f2dd2b9dad907104fca798662
Georgios Natrosvili <100809251+Natrosvili@users.noreply.github.com>
2022-09-09 17:46:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,40 @@
 SCORE = 0
-FRAGEN = 3
+QUESTIONS = 3
 
 print("Hallo! Willkommen zum multiple Choice Quiz")
 print("FRAGE Nu.1:")
 ant1 = input("welches Handy habe ich ?\na. Huawei\nb. Samsung\nc. Xiaomi\nd. Iphone\nAntwort:")
-if ant1 == "c" or ant1 == "Xiaomi" or ant1 == "xiaomi":
+if ant1 == "C" or ant1 == "c" or ant1 == "Xiaomi" or ant1 == "xiaomi":
     SCORE += 1
     print("Richtig!")
-    print("Du hast: " + str(SCORE) + "/" + FRAGEN + " richtig.\n")
+    print("Du hast: " + str(SCORE) + "/" + str(QUESTIONS) + " richtig.\n")
 else:
     print("Falsch! die richtige Antwort war -Xiaomi.\n")
 
 
 print("Frage Nu.2:")
 ant2 = input("Welchen Computer habe Ich?\na. Mac\nb. Acer\nc. Samsung\nAntwort:")
-if ant2 == "b" or ant2 == "Acer" or ant2 == "acer":
+if ant2 == "B" or ant2 == "b" or ant2 == "Acer" or ant2 == "acer":
     SCORE += 1
     print("Richtig!")
-    print("Du hast: " + str(SCORE) + "/" + FRAGEN + "richtig.\n")
+    print("Du hast: " + str(SCORE) + "/" + str(QUESTIONS) + " richtig.\n")
 else:
     print("Falsch!\nDie richtige Antwort war -Acer.\n")
 
 
 print("Frage Nu.3")
 ant3 = input("Welche Programmsprache gefällt mir am besten?\na. Python\nb. JavaScript\nc. Java\nAntwort:")
-if ant3 == "a" or ant3 == "Python" or ant3 == "python":
+if ant3 == "A" or ant3 == "a" or ant3 == "Python" or ant3 == "python":
     SCORE += 1
     print("Richtig!")
-    print("Du hast: " + str(SCORE) + "/" + FRAGEN + " richtig.\n")
+    print("Du hast: " + str(SCORE) + "/" + str(QUESTIONS) + " richtig.\n")
 else:
     print("Falsch!\nDie richtige Antwort war -Python.\n")
 
 
 if SCORE <= 1:
-    print(str(SCORE) + "/" + FRAGEN + "-you suck.")
+    print(str(SCORE) + "/" + str(QUESTIONS) + "-you suck.")
 elif SCORE == 2:
-    print(str(SCORE) + "/" + FRAGEN + " -Ausreichend.")
+    print(str(SCORE) + "/" + str(QUESTIONS) + " -Ausreichend.")
 else:
-    print("Herzlichen Glückwünsch! Du hast " + str(SCORE) + "/" + FRAGEN + " richtig.")
-
+    print("Herzlichen Glückwünsch! Du hast " + str(SCORE) + "/" + str(QUESTIONS) + " richtig.")
